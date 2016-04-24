043e9b4d83e3db13b55ddbcbe8c6499fc3d7ed18
lstrichashl <lstrichashl@gmail.com>
2016-04-24 13:14:16 +0300

    add uuid in c#

--- CSharp/CSharp/Controllers/FilesController.cs
+++ CSharp/CSharp/Controllers/FilesController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
@@ -17,18 +20,30 @@ namespace server.Controllers
             var httpRequest = HttpContext.Current.Request;
             if (httpRequest.Files.Count > 0)
             {
-                foreach (string file in httpRequest.Files)
-                {
-                    var postedFile = httpRequest.Files[file];
-                    var filePath = HttpContext.Current.Server.MapPath("~/uploads/" + postedFile.FileName);
-                    postedFile.SaveAs(filePath);
-                    // NOTE: To store in memory use postedFile.InputStream
-                }
-                
-                return Request.CreateResponse(HttpStatusCode.NoContent);
+                var uploadResult = SaveFile(httpRequest);
+                return Request.CreateResponse(HttpStatusCode.OK, uploadResult);
             }
-
             return Request.CreateResponse(HttpStatusCode.BadRequest);
         }
+
+        private static UploadResult SaveFile(HttpRequest httpRequest)
+        {
+            httpRequest.Files.Get(0);
+            var guid = System.Guid.NewGuid().ToString();
+            var postedFile = httpRequest.Files.Get(0);
+            var DirectoryPath = HttpContext.Current.Server.MapPath("~/uploads");
+            if (!Directory.Exists(DirectoryPath))
+            {
+                Directory.CreateDirectory(DirectoryPath);
+            }
+            var filePath = DirectoryPath + '\\' + guid;
+            postedFile.SaveAs(filePath);
+            return new UploadResult() { file_name = guid };
+        }
+    }
+
+    public class UploadResult
+    {
+        public string file_name { get; set; }
     }
 }
